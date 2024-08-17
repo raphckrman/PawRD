0e167b3b70300dea921893f013581e6cb88aefd2
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-17 18:09:42 +0200

    fix(finders): gec data not using first `{`

--- src/utils/finders.ts
+++ src/utils/finders.ts
@@ -1,14 +1,10 @@
+import { findValueBetween } from "@literate.ink/utilities";
+
 /**
  * Data on a route is contained into that single
  * variable in a script tag. Content is encoded in JSON, as a string.
  */
 export const findAndReadGecData = <T extends {} = {}>(html: string): T => {
-  const value = findValueBetween(html, "var gecData = {", "</script");
+  const value = findValueBetween(html, "var gecData = ", "</script");
   return JSON.parse(value);
-};
-
-export const findValueBetween = (plain: string, start: string, end: string): string => {
-  const startIndex = plain.indexOf(start) + start.length;
-  const endIndex = plain.indexOf(end, startIndex);
-  return plain.slice(startIndex, endIndex);
 };
